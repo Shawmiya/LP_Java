2a2d7a162400a368172c518abdbd6bd75e9c994b
Shawmiya <yoshawmi94@gmail.com>
2020-02-06 22:11:39 +0530

    find factorial using recursive algorithm

--- Factorial/Factorial.java
+++ Factorial/Factorial.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 public class Factorial
 {
-    
     static int fact(int x)
     {
         if(x == 0)
-        {
             return 1;
-        }
+		
 		else
 			return(x * fact(x - 1));
         
