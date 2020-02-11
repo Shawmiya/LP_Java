f3c14c10d04ca90270db957d76df7ecddecb8881
Shawmiya <yoshawmi94@gmail.com>
2020-02-11 07:00:53 +0530

    entity relationship with springboot

--- employeeService-relationship/src/main/java/com/example/employeeService/EmployeeServiceApplication.java
+++ employeeService-relationship/src/main/java/com/example/employeeService/EmployeeServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeService1Application {
+public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeService1Application.class, args);
+		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
 }
